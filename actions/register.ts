7a47c4f35a9c61adbd8a51a306a78302d0fe045b
SameerHRV <sameerharapanahalli2004@gmail.com>
2024-07-03 17:13:29 +0530

    add register functionlaty

--- actions/register.ts
+++ actions/register.ts
@@ -1,18 +1,42 @@
 "use server";
 import * as z from "zod";
+import bcryptjs from "bcryptjs";
 
+import { db } from "@/lib/db";
 import { registerSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
-  const validaedSchema = registerSchema.safeParse(values);
+  const validatedFields = registerSchema.safeParse(values);
 
-  if (!validaedSchema.success) {
+  if (!validatedFields.success) {
     return {
       error: "Invalid credentials",
     };
   }
 
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcryptjs.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return {
+      error: "User already exists",
+    };
+  }
+
+  await db.user.create({
+    data: {
+      name: name.toLowerCase(),
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO: Send email verification
+
   return {
-    success: "Register successful",
+    success: "User registered successfully",
   };
 };
